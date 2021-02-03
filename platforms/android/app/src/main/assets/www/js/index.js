9db6387d24434e89981b1f19bf62fda59a1af124
SrijanKhare1496 <khare.srijan1496@gmail.com>
2021-02-03 17:17:52 +0530

    platform added

---
+++ platforms/android/app/src/main/assets/www/js/index.js
@@ -0,0 +1,44 @@
+
+document.addEventListener('deviceready', onDeviceReady, false);
+
+function onDeviceReady() {
+//     // Cordova is now initialized. Have fun!
+
+//     let map;
+
+// function initMap() {
+//   map = new google.maps.Map(document.getElementById("map"), {
+//     center: { lat: -34.397, lng: 150.644 },
+//     zoom: 8,
+//   });
+// }
+
+navigator.geolocation.getCurrentPosition(onSuccess, onError, {
+  timeout: 30000 , enableHighAccuracy:timeout
+});
+
+function onSuccess(position) {
+  var lat = position.coords.latitude;
+  var lang = position.coords.longitude;
+
+  //Google Maps
+  var myLatlng = new google.maps.LatLng(lat, lang);
+  var mapOptions = {
+      zoom: 15,
+      center: myLatlng,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+  }
+  var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+  var marker = new google.maps.Marker({
+      position: myLatlng,
+      map: map
+  });
+}
+
+function onError(error) {
+  alert('code: ' + error.code + '\n' +
+      'message: ' + error.message + '\n');
+}
+google.maps.event.addDomListener(window, 'load', onSuccess);
+
+}
